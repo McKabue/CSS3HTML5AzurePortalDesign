4c25fa1c9b5452fce9892d604165410d7bcb9635
Charles <mckabue@gmail.com>
2017-01-15 22:43:00 +0300

    Initial Commit...

---
+++ wwwroot/app/script/JavaScript.js
@@ -0,0 +1,7 @@
+﻿var i = 0;
+
+$('#add').click(function () {
+    $('.container').append('<div class="box"></div>');
+    var ddd = $('.box:last-child');
+   // $('.container').scrollLeft(ddd[0].offsetLeft);
+});
